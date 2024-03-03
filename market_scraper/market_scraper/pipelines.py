b5259b4413e182c56b3e38fd1b5e5d0d76a3bbe2
Selebogo Mosoeu <scmosoeu@gmail.com>
2024-03-03 13:12:33 +0200

    convert to lowercase

--- market_scraper/market_scraper/pipelines.py
+++ market_scraper/market_scraper/pipelines.py
@@ -10,4 +10,15 @@ from itemadapter import ItemAdapter
 
 class MarketScraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        # Convert to lowercase
+        string_fields = ['commodity', 'container', 'product_combination']
+        for field_name in field_names:
+            if field_name in string_fields:
+                value = adapter.get(field_name)
+                adapter[field_name] = value.lower()
+
         return item
